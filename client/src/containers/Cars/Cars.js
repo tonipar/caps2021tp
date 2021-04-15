c53435b293e80262385efc0a26f1c5ca2de0195f
Antti Vikman <antti.vikman@nolwenture.com>
2021-04-15 09:31:32 +0300

    Task 4

--- client/src/containers/Cars/Cars.js
+++ client/src/containers/Cars/Cars.js
@@ -4,15 +4,20 @@ import Car from "./Car";
 import "./Cars.css";
 
 export default function Cars(props) {
+  const cars = [
+    { id: 1, make: "ford", model: "Mustang" },
+    { id: 2, make: "honda", model: "Civic" },
+    { id: 3, make: "skoda", model: "Octavia" },
+    { id: 4, make: "honda", model: "Accord" },
+    { id: 5, make: "skoda", model: "Fabia" },
+    { id: 6, make: "ford", model: "T" },
+    { id: 7, make: "volvo", model: "V40" },
+  ];
   return (
     <Row id="cars">
-      <Car make="ford" model="Mustang" />
-      <Car make="honda" model="Civic" />
-      <Car make="skoda" model="Octavia" />
-      <Car make="honda" model="Accord" />
-      <Car make="skoda" model="Fabia" />
-      <Car make="ford" model="T" />
-      <Car make="volvo" model="V40" />
+      {cars.map((car) => (
+        <Car key={car.id} make={car.make} model={car.model} />
+      ))}
     </Row>
   );
 }
