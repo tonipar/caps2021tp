4e39e447784a34f2b564ab69d7110630210363b8
Antti Vikman <antti.vikman@nolwenture.com>
2021-05-06 15:22:57 +0300

    Adds Fruits POST and DELETE endpoints

--- javabackend/demo/src/main/java/com/example/demo/controllers/FruitsController.java
+++ javabackend/demo/src/main/java/com/example/demo/controllers/FruitsController.java
@@ -6,18 +6,33 @@ import java.util.List;
 import com.example.demo.models.Fruit;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class FruitsController {
 
     @GetMapping("/Fruits")
-    public List<Fruit> index() {
+    public List<Fruit> getFruits() {
         ArrayList<Fruit> fruits = new ArrayList<Fruit>();
-        fruits.add(new Fruit("banana", "Caverion"));
-        fruits.add(new Fruit("pear", "Concorde"));
-        fruits.add(new Fruit("apple", "Machintosh"));
+        fruits.add(new Fruit(1l, "banana", "Caverion"));
+        fruits.add(new Fruit(2l, "pear", "Concorde"));
+        fruits.add(new Fruit(3l, "apple", "Machintosh"));
         return fruits;
+    }
+
+    @PostMapping("/Fruits")
+    public Fruit addFruit(@RequestBody Fruit newFruit) {
+        System.out.println(newFruit);
+        return newFruit;
+    }
+
+    @DeleteMapping("/Fruits/{id}")
+    public void deleteFruit(@PathVariable Long id) {
+        System.out.println("DELETE Fruit with id " + id);
     }
 
 }
--- javabackend/demo/src/main/java/com/example/demo/models/Fruit.java
+++ javabackend/demo/src/main/java/com/example/demo/models/Fruit.java
@@ -1,12 +1,18 @@
 package com.example.demo.models;
 
 public class Fruit {
+    private Long id;
     private String type;
     private String name;
 
-    public Fruit(String type, String name) {
+    public Fruit(Long id, String type, String name) {
+        this.id = id;
         this.type = type;
         this.name = name;
+    }
+
+    public Long getId() {
+        return this.id;
     }
 
     public String getType() {
@@ -15,5 +21,9 @@ public class Fruit {
 
     public String getName() {
         return this.name;
+    }
+
+    public String toString() {
+        return "Fruit - type:" + this.type + ", name:" + this.name;
     }
 }
