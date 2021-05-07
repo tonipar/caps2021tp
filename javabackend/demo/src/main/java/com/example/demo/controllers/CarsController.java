bdd0a336d6033208f432299447ef538b2f5fb792
Antti Vikman <antti.vikman@nolwenture.com>
2021-05-07 09:25:54 +0300

    Adds persistent endpoints to Cars

--- javabackend/demo/src/main/java/com/example/demo/controllers/CarsController.java
+++ javabackend/demo/src/main/java/com/example/demo/controllers/CarsController.java
@@ -4,19 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.example.demo.models.Car;
+import com.example.demo.repositories.CarsRepository;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class CarsController {
+    private final CarsRepository repository;
+
+    CarsController(CarsRepository repository) {
+        this.repository = repository;
+    }
 
     @GetMapping("/Cars")
     public List<Car> index() {
-        ArrayList<Car> cars = new ArrayList<Car>();
-        cars.add(new Car(1l, "ford", "Mustang"));
-        cars.add(new Car(2l, "honda", "Civic"));
-        return cars;
+        return this.repository.findAll();
     }
 
+    @PostMapping("/Cars")
+    public Car addCar(@RequestBody Car newCar) {
+        return this.repository.save(newCar);
+    }
+
+    @DeleteMapping("/Cars/{id}")
+    public void deleteFruit(@PathVariable Long id) {
+        this.repository.deleteById(id);
+    }
 }
