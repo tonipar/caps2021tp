3be78850e7ca3e3b3de600e89cfd0d7d10041ab2
Antti Vikman <antti.vikman@nolwenture.com>
2021-05-07 16:05:35 +0300

    Adds PATCH and PUT to Car endpoints

--- javabackend/demo/src/main/java/com/example/demo/controllers/CarsController.java
+++ javabackend/demo/src/main/java/com/example/demo/controllers/CarsController.java
@@ -6,10 +6,15 @@ import com.example.demo.models.Car;
 import com.example.demo.repositories.CarsRepository;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -25,13 +30,41 @@ public class CarsController {
         return this.repository.findAll();
     }
 
+    @GetMapping("/Cars/{id}")
+    public Car getCar(@PathVariable Long id) {
+        return this.repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Could not entity with id " + id));
+    }
+
     @PostMapping("/Cars")
     public Car addCar(@RequestBody Car newCar) {
         return this.repository.save(newCar);
     }
 
+    @PutMapping("/Cars/{id}")
+    public Car replaceCar(@RequestBody Car newCar, @PathVariable Long id) {
+        newCar.setId(id);
+        return this.repository.save(newCar);
+    }
+
+    @PatchMapping("/Cars/{id}")
+    public Car patchCar(@RequestBody Car newCar, @PathVariable Long id) {
+        Car oldCar = this.getCar(id);
+        if (newCar.getMake() != null) {
+            oldCar.setMake(newCar.getMake());
+        }
+        if (newCar.getModel() != null) {
+            oldCar.setModel(newCar.getModel());
+        }
+        return this.repository.save(oldCar);
+    }
+
     @DeleteMapping("/Cars/{id}")
-    public void deleteFruit(@PathVariable Long id) {
-        this.repository.deleteById(id);
+    public void deleteCar(@PathVariable Long id) {
+        try {
+            this.repository.deleteById(id);
+        } catch (DataAccessException error) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Could not entity with id " + id);
+        }
     }
 }
--- javabackend/demo/src/main/java/com/example/demo/models/Car.java
+++ javabackend/demo/src/main/java/com/example/demo/models/Car.java
@@ -5,29 +5,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Car {
     private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
     private String make;
     private String model;
-
-    Car() {
-    }
-
-    public Car(String make, String model) {
-        this.make = make;
-        this.model = model;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public String getMake() {
-        return this.make;
-    }
-
-    public String getModel() {
-        return this.model;
-    }
 }
