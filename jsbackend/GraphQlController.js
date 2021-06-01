074aac16d77f17caee975e8e3ec180c543acb058
Antti Vikman <antti.vikman@nolwenture.com>
2021-06-01 09:02:43 +0300

    GraphQL for Cars

--- jsbackend/GraphQlController.js
+++ jsbackend/GraphQlController.js
@@ -4,13 +4,16 @@ import { graphqlHTTP } from "express-graphql";
 const schemaComposer = new SchemaComposer();
 
 import { FruitQuery, FruitMutation } from "./fruits/FruitsGraphQLContorller.js";
+import { CarQuery, CarMutation } from "./cars/CarsGraphQLContorller.js";
 
 schemaComposer.Query.addFields({
   ...FruitQuery,
+  ...CarQuery,
 });
 
 schemaComposer.Mutation.addFields({
   ...FruitMutation,
+  ...CarMutation,
 });
 
 export default (app) => {
