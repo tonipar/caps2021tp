90fdc59f3fe5bacb69db6bfb5d0a4aae29b0702c
Antti Vikman <antti.vikman@nolwenture.com>
2021-06-03 08:54:37 +0300

    GraphQL join

--- jsbackend/fruits/FruitSchema.js
+++ jsbackend/fruits/FruitSchema.js
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
+export const FruitColorSchema = new Schema({
+  color: { type: String, enum: ["red", "green", "orange"] },
+  fruits: [{ type: Schema.Types.ObjectId, ref: "Fruit" }],
+});
+
 export default new Schema({
   type: { type: String, enum: ["apple", "pear", "orange"] },
   name: { type: String },
   comment: { type: String },
+  color: { type: Schema.Types.ObjectId, ref: "FruitColor" },
 });
